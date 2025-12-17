a6a05712cf1f33c5d945772fd15bd63431ae4ee4
YohannesC01 <yohannessigan2@gmail.com>
2025-12-17 13:08:18 +0800

    lab 02

---
+++ labs/lab02/exercise5/exercise5.py
@@ -0,0 +1,6 @@
+def is_valid_triangle(side1,side2,side3):
+    if side1 + side2 > side3:
+        return True
+    else:
+        return False
+    
--- labs/lab02/exercise8/exercise8.py
+++ labs/lab02/exercise8/exercise8.py
@@ -4,12 +4,15 @@
 def calculate_bounce_height(current_height):
     """Calculate next bounce height (80% of current)."""
     # TODO: Implement this function
-    pass
+    return current_height * 0.8
 
 def is_ball_stopped(height):
     """Return True if height < 1, False otherwise."""
     # TODO: Implement this function
-    pass
+    if height < 1:
+        return True
+    else:
+        return False
 
 def simulate_bouncing_ball(start_height):
     """
@@ -17,7 +20,17 @@ def simulate_bouncing_ball(start_height):
     Returns: (bounce_count, total_distance)
     """
     # TODO: Implement using calculate_bounce_height and is_ball_stopped
-    pass
+    bounces = 0 
+    height = start_height
+    total_distances = start_height
+    current_height = start_height
+
+    while not is_ball_stopped(height):
+        distance = calculate_bounce_height(current_height)
+        bounces += 1
+        total_distances += 2 * distance
+
+    return total_distances, bounces
 
 # Test your code here
 print("Testing Bouncing Ball Simulation...")
