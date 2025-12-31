bce429f7a18c53e603b0c78dc5e74d5721ab3482
YohannesC01 <yohannessigan2@gmail.com>
2025-12-31 13:05:21 +0800

    lab 03

--- labs/lab03/exercise3/exercise3.py
+++ labs/lab03/exercise3/exercise3.py
@@ -1,2 +1,6 @@
 def has_adjacent_seats(seats):
-    pass
+    for i in range(0,len(seats)-1):
+        if seats[i+1] == 0 and seats[i-1] == 0:
+            return True
+        else: 
+            return False
