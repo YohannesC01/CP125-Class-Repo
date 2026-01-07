afccfd3d753cd631faaa8362ced86a92b5f8a46f
YohannesC01 <yohannessigan2@gmail.com>
2026-01-07 13:05:52 +0800

    lab04

--- labs/lab04/exercise4/exercise4.py
+++ labs/lab04/exercise4/exercise4.py
@@ -1,7 +1,24 @@
 
 def analyze_performance(lap_times):
-  
-    pass
+  firstlap=[]
+  secondlap=[]
+  for i in range (0,(len(lap_times)-1)):
+    if len(lap_times)%2 == 0:
+        if i+1 <= len(lap_times) // 2 == 0:
+            firstlap.append(lap_times[i])
+        else:
+            i+1 <= len(lap_times) // 2 == 0 
+            secondlap.append(lap_times[i])
+    else:
+        if i+1 <= (len(lap_times) // 2)+1:
+            firstlap.append(lap_times[i])
+        else:
+            i+1 <= (len(lap_times) // 2) 
+            secondlap.append(lap_times[i])
+        
+
+average_firstlap = sum(firstlap)/len(firstlap)
+
 
 
 # Test
