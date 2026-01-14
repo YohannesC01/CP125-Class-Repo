74f8fd7c777801f8b8afe15b59c0b1a362000931
YohannesC01 <yohannessigan2@gmail.com>
2026-01-14 13:11:49 +0800

    lab 5

--- labs/lab05/exercise3/exercise3.py
+++ labs/lab05/exercise3/exercise3.py
@@ -3,8 +3,20 @@ def find_bottleneck_index(traceroute):
     """
     Find the index of the hop where the largest latency jump begins.
     """
-    pass
+    max_jump = 0
+    bottleneck_jump_index = 0 
 
+    for i in range (1,len(traceroute)):
+        previous = traceroute[i-1][1]
+        current = traceroute [i][1]
+
+        jump = abs(current - previous)
+
+        if jump > max_jump:
+            max_jump = jump
+            bottleneck_jump_index = i - 1
+
+    return bottleneck_jump_index
 
 # Test
 traceroute = ((1, 5), (2, 8), (3, 45), (4, 48), (5, 50))
--- labs/lab05/exercise4/exercise4.py
+++ labs/lab05/exercise4/exercise4.py
@@ -1,10 +1,27 @@
-
-def filter_query_times(times):
+def filter_query_times(query_times):
     """
     Remove slow outliers (mean + std deviation) and return sorted times.
     """
-    pass
+    if not query_times:
+        return[]
+        
+    mean =  sum(query_times) / len(query_times)
 
+    for i in range (len(query_times)):
+        sum = 0 
+        varience = sum(i-mean)**2 / len(query_times)
+        sum+=varience
+
+    std_dev = (varience)**0.5
+
+    upper_limit = mean + std_dev
+
+    
+    for i in query_times[:]:
+        if i > upper_limit:
+            query_times.remove(i)
+            
+    return query_times 
 
 # Test
 query_times = [45, 52, 48, 180, 51, 47, 50, 12]
